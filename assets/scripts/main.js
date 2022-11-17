e5760a1f818126eb92065ef38a5f4ab866a66f69
Wilian-N-Silva <wilian.silva209@gmail.com>
2022-11-17 18:11:51 -0300

    feat: i18n texts and icons

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,3 @@
-
-// sem fundo no topo
-
-// colorir o fundo ao expandir independente de onde esteja
-
-// colorir o fundo ao descer a página
-
 const header = document.querySelector('header');
 const navbar = header.querySelector('nav')
 const btn_navigation = document.getElementById('btn-navigation-toggle')
@@ -22,7 +15,6 @@ function handleNavbarTransparency() {
       header.classList.add('bg-active');
     }
   }
-
 }
 
 function handleNavbar() {
@@ -32,3 +24,60 @@ function handleNavbar() {
 
 btn_navigation.addEventListener('click', handleNavbar)
 document.addEventListener('scroll', handleNavbarTransparency)
+
+const supportedLocales = ['pt', 'en']
+
+const languageSwitch = document.querySelector("#btn-language-selector")
+
+let translation
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   languageSwitch.replaceChildren("")
+
+//   Object.entries(supportedLocales).forEach(([key, value]) => {
+//     const option = document.createElement("option")
+//     option.value = key
+//     option.innerText = value
+//     languageSwitch.append(option)
+//   })
+// })
+
+languageSwitch.addEventListener("click", (event) => {
+  const newLocale = languageSwitch.dataset.i18nCurrent === 'pt' ? 'en' : 'pt'
+  languageSwitch.dataset.i18nCurrent = newLocale
+
+  const buttonSpan = languageSwitch.querySelectorAll('span[data-i18n-option]')
+
+  for (let index = 0; index < buttonSpan.length; index++) {
+    buttonSpan[index].classList.remove('active')
+
+    if (buttonSpan[index].dataset.i18nOption === newLocale) {
+      buttonSpan[index].classList.add('active')
+    }
+  }
+
+  changeLocale(newLocale)
+})
+
+async function fetchTranslationOf(locale) {
+  const langPath = `./assets/lang/${locale}.json`
+  const response = await fetch(langPath)
+
+  return await response.json()
+}
+
+async function changeLocale(newLocale) {
+  translation = await fetchTranslationOf(newLocale)
+  translatePage()
+}
+
+function translatePage() {
+  const i18nKeys = document.querySelectorAll("[data-i18n-key]")
+
+  document.documentElement.lang = `${translation['language-code']}-${translation['country-code']}`
+
+  i18nKeys.forEach((element) => {
+    const key = element.getAttribute('data-i18n-key')
+    element.innerHTML = translation['strings'][key]
+  })
+}
